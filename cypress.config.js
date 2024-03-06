37c5b3f3b8f53bd9114e0aba8177cd5d1cb9c907
Karina Karpinskaya <karinakarpinskaya72@gmail.com>
2024-03-06 15:47:19 +0200

    test_testomat_x_cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("@testomatio/reporter/lib/adapter/cypress-plugin")(on, config);
     },
   },
 };
