9305c3e3984e85444aec4c015701f1aee310ae95
Karina Karpinskaya <karinakarpinskaya72@gmail.com>
2024-03-07 19:59:19 +0200

    test

--- cypress/e2e/order.cy.js
+++ cypress/e2e/order.cy.js
@@ -9,7 +9,7 @@ describe("Order suite", () => {
     orderPage.login(user.loginname, user.password);
 
     // Act
-    // orderPage.searchForProduct("Waterproof Protective Undereye Concealer");
+    orderPage.searchForProduct("Waterproof Protective Undereye Concealer");
     orderPage.selectProductOption("656");
     cy.wait(1000);
     cy.intercept(apiEndpoints.get_option_resources).as("getOptionResources");
