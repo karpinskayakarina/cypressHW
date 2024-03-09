d08f6c658f1dde871ec0e5c88366551dda8b0b12
Karina Karpinskaya <karinakarpinskaya72@gmail.com>
2024-03-09 21:48:45 +0200

    tets

--- cypress/e2e/authorization.cy.js
+++ cypress/e2e/authorization.cy.js
@@ -1,24 +1,41 @@
 import user from "../fixtures/user.json";
+import { fillAuthorizationFields } from "../support/helper";
 import LoginPage from "../support/pages/LoginPage";
 
 const loginPage = new LoginPage();
 
 describe("Authorization positive scenarios", () => {
-  it("Authorization by HTTP", () => {
-    cy.authenticate(user.loginname, user.password);
+  it("Authorization", () => {
+    loginPage.visitLoginPage();
+
+    fillAuthorizationFields(user.loginname, user.password);
+    loginPage.verifyUserFirstNameDisplayed(user.firstname);
   });
 });
 
 describe("Authorization negative scenarios", () => {
-  it("Authorization without entered password by HTTP", () => {
-    cy.authenticate(user.loginname, "");
+  it("Authorization without entered username", () => {
+    loginPage.visitLoginPage();
+    fillAuthorizationFields("", user.password);
+
+    loginPage.verifyErrorMessageDisplayed(
+      "Error: Incorrect login or password provided."
+    );
   });
 
-  it("Authorization without entered username by HTTP", () => {
-    cy.authenticate("", user.password);
+  it("Authorization without entered password", () => {
+    loginPage.visitLoginPage();
+    fillAuthorizationFields(user.loginname);
+    loginPage.verifyErrorMessageDisplayed(
+      "Error: Incorrect login or password provided."
+    );
   });
 
-  it("Authorization with empty fields by HTTP", () => {
-    cy.authenticate("", "");
+  it("Authorization with empty fields", () => {
+    loginPage.visitLoginPage();
+    fillAuthorizationFields();
+    loginPage.verifyErrorMessageDisplayed(
+      "Error: Incorrect login or password provided."
+    );
   });
 });
